0732180ff2f4afc96739c1f717d56c1e7bbe1250
zhengchun <zhengchunster@gmail.com>
2017-12-01 14:34:31 +0800

    test: add basic auth test

--- proxy_test.go
+++ proxy_test.go
@@ -1,6 +1,7 @@
 package antch
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,6 +10,7 @@ import (
 	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 	"sync"
 	"testing"
 )
@@ -25,22 +27,55 @@ func testProxyHandler(t *testing.T, proxyURL *url.URL) {
 		},
 	}
 
-	handler := proxyHandler(http.ProxyURL(proxyURL), backMessageHandler(client))
+	handler := ProxyMiddleware(http.ProxyURL(proxyURL))(backMessageHandler(client))
 	req, _ := http.NewRequest("GET", ts.URL, nil)
 	resp, err := handler.Send(req)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer resp.Body.Close()
-
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatalf("ReadAll: %v", err)
 	}
 	if g, e := string(b), "hello world"; g != e {
-		t.Error("expected %s; got %s", e, g)
+		t.Errorf("expected %s; got %s", e, g)
 	}
 }
+
+func TestHTTPAuthProxyHandler(t *testing.T) {
+	// HTTP proxy server with authentication.
+	var (
+		username = "test"
+		password = "test"
+	)
+	proxyServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "CONNECT" {
+			auth := strings.SplitN(r.Header.Get("Proxy-Authorization"), " ", 2)
+			if len(auth) != 2 && auth[0] != "Basic" {
+				w.WriteHeader(401)
+				return
+			}
+			payload, _ := base64.StdEncoding.DecodeString(auth[1])
+			if pair := strings.Split(string(payload), ":"); len(pair) != 2 || !(pair[0] == username && pair[1] == password) {
+				w.WriteHeader(401)
+				return
+			}
+		}
+		director := func(req *http.Request) {
+			req.URL.Host = r.Host
+			req.URL.Scheme = "http"
+		}
+		proxy := &httputil.ReverseProxy{Director: director}
+		proxy.ServeHTTP(w, r)
+	}))
+	defer proxyServer.Close()
+
+	proxyURL, _ := url.Parse(proxyServer.URL)
+	proxyURL.User = url.UserPassword(username, password)
+	testProxyHandler(t, proxyURL)
+}
+
 func TestHTTPProxyHandler(t *testing.T) {
 	// HTTP proxy server.
 	proxyServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
